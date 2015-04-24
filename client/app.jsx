803a10cfae4e576bfecccd507bf42c58c838732f
Austen Talbot <austentalbot@gmail.com>
2015-04-23 21:04:53 -0700

    More basic setup

--- client/app.jsx
+++ client/app.jsx
@@ -1,1 +1,9 @@
-var google = require('googleapis');
+var React = require('react');
+var R = React.createElement;
+var reqwest = require('reqwest');
+
+React.render(R('button', {
+  onClick: function() {
+    console.log('send the email');
+  }
+}, 'send an email'), document.getElementById('react'));
